969a2bde6ded064661ea1913998ac71e54f88792
= <perezlaras@covatrix.com>
2022-06-03 22:13:22 -0400

    minimal api example

---
+++ minimal-api-sample/Constants/WeatherConstants.cs
@@ -0,0 +1,9 @@
+namespace minimal_api_sample.Constants;
+
+public static class WeatherConstants
+{
+    public static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+}
---
+++ minimal-api-sample/Program.cs
@@ -0,0 +1,45 @@
+using minimal_api_sample;
+using minimal_api_sample.Constants;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//APIs
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           WeatherConstants.Summaries[Random.Shared.Next(WeatherConstants.Summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+
+app.Run();
